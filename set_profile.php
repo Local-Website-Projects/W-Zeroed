fc55b7ba903653c62eb290f159285054b6af4fe7
Mugdho <saha2171515@stud.kuet.ac.bd>
2025-01-27 17:03:02 +0600

    27/01/25

--- set_profile.php
+++ set_profile.php
@@ -9,13 +9,9 @@ if(!isset($_SESSION['seller_id'])){
 }
 
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
-<!-- Mirrored from freelanhub.vercel.app/candidates-payouts.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 25 Jan 2025 07:12:43 GMT -->
-<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -455,9 +451,6 @@ if(!isset($_SESSION['seller_id'])){
                             </select>
                         </div>
                     </div>
-
-
-
 
                     <div class="flex items-center col-span-full gap-5 mt-5">
                         <button class="button-main -border">Reset</button>
